cde88c676ccae5e7cc3e939acc06a981c7178327
Manish_sharma_attri <102635250+TheHemanUltra@users.noreply.github.com>
2023-08-08 07:48:42 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,17 @@
+var items=document.getElementsByClassName('list-group-item')
+console.log(items)
+var headertitle=document.getElementById('header-title')
+
+headertitle.style.borderBottom='solid 3px #000'
+
+
+
+var demo=document.getElementById('main')
+console.log(demo)
+
+demo.style.fontStyle='bold'
+demo.style.backgroundColor='green'
+
+
+
+
