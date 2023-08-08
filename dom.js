bb589a5f8eecd82e8aee834950df568aef574365
Manish_sharma_attri <102635250+TheHemanUltra@users.noreply.github.com>
2023-08-08 21:45:46 +0530

    Add files via upload

--- dom.js
+++ dom.js
@@ -1,10 +1,16 @@
-var items=document.getElementsByClassName('list-group-item')
-console.log(items)
-console.log(items[1])
+// var items=document.getElementsByClassName('list-group-item')
+// console.log(items)
+// console.log(items[1])
 
-items[2].style.backgroundColor='green'
-// items[0].style.fontWeight='bold'
+// items[2].style.backgroundColor='green'
+// // items[0].style.fontWeight='bold'
 
-for(i=0;i<items.length;i++){
-    items[i].style.fontWeight='bold'
-}
+// for(i=0;i<items.length;i++){
+//     items[i].style.fontWeight='bold'
+// }
+
+var seconditem=document.querySelector('.list-group-item:nth-child(2)')
+seconditem.style.color='green'
+
+var seconditem=document.querySelector('.list-group-item:nth-child(3)')
+seconditem.style.color='white'
